95d29b7322d3e3bdd567160d0d8fc4d1a7bd93a4
troyhe <46601555+troyhe@users.noreply.github.com>
2021-07-03 07:07:58 -0400

    Create 0704_Binary Search.cpp

---
+++ 0704_Binary Search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0; 
+        int right = nums.size();
+        while (left < right){
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+};
